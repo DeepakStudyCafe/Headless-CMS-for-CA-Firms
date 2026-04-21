df140562802e8121629b7c1038ea5946874a961e
Deepak Sharma <sdeepakncy@gmail.com>
2026-04-21 13:24:52 +0530

    add more template 18

--- websites/Template-16/dist/server/assets/_tanstack-start-manifest_v-DfAHp_xr.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/user/Desktop/StudyCafe/Headless-CMS-for-CA-Firms/websites/Template-16/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-BlB-CK2s.js"] }, "/": { filePath: "C:/Users/user/Desktop/StudyCafe/Headless-CMS-for-CA-Firms/websites/Template-16/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-Du0L35LZ.js"] } }, clientEntry: "/assets/index-BlB-CK2s.js" });
-export {
-  tsrStartManifest
-};
--- websites/Template-18/dist/server/assets/_tanstack-start-manifest_v-vH4tgiOs.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/user/Desktop/StudyCafe/Headless-CMS-for-CA-Firms/websites/Template-18/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-CjAs-pFQ.js"] }, "/": { filePath: "C:/Users/user/Desktop/StudyCafe/Headless-CMS-for-CA-Firms/websites/Template-18/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-Cja84Q4x.js"] } }, clientEntry: "/assets/index-CjAs-pFQ.js" });
-export {
-  tsrStartManifest
-};
